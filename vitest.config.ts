46449cb4473d97e7f375c6b0eebdb86515a3ae12
justinlin9299 <136868011+justinlin9299@users.noreply.github.com>
2026-05-20 16:28:20 +0800

    ci: add CI pipeline and update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,16 @@
-import type { TestUserConfig, ViteUserConfig } from 'vitest/config';
+import type { TestUserConfig, ViteUserConfig } from 'vitest/config'
 
 type VitestConfig = ViteUserConfig & {
-  test?: TestUserConfig;
-};
+  test?: TestUserConfig
+}
 
 const config: VitestConfig = {
   test: {
-    exclude: ['dist/**', 'node_modules/**']
-  }
-};
+    exclude: ['dist/**', 'node_modules/**'],
+    reporters: process.env.CI
+      ? ['verbose', ['junit', { outputFile: 'test-results/junit.xml' }]]
+      : ['verbose'],
+  },
+}
 
-export default config;
+export default config
